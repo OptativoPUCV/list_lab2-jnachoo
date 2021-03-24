ef48397b7feebd177d080792ef3529f03aebc2b1
Replit user <replituser@example.com>
2021-03-24 23:26:18 +0000

    new try

--- list.c
+++ list.c
@@ -44,7 +44,8 @@ void * firstList(List * list) {
 
 void * nextList(List * list) {
   if(list->head == NULL)return NULL;
-  Node* aux = list->current; 
+  Node* aux =malloc(sizeof(Node));
+   aux=list->current; 
   list->current=aux->next;
     return (int*)(list->current->data);
 }
