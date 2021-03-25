c1c410602fe8427cda7e314b74f88bbe5bf06f23
Replit user <replituser@example.com>
2021-03-25 17:13:01 +0000

    new try

--- list.c
+++ list.c
@@ -71,7 +71,7 @@ void pushFront(List * list, const void * data) {
   nodo->next=list->head;
   if(list->head)
   //if(list->head == list->tail)
-  printf("cola y cabeza ==");
+  //printf("cola y cabeza ==");
   list->head->prev = nodo;
   list->head = nodo;
   nodo->prev= NULL;
